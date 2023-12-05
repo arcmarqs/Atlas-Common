18ba52110f15e9b65a564cad97765bb4a4f46e55
Amadeu Marques <maramadeu@gmail.com>
2023-12-05 17:28:33 +0000

    sled db

--- src/persistentdb/mod.rs
+++ src/persistentdb/mod.rs
@@ -2,12 +2,15 @@ use std::path::Path;
 use std::sync::Arc;
 use thiserror::Error;
 
+use ::sled::IVec;
+
 use crate::error::*;
 
 #[cfg(feature = "persistent_db_rocksdb")]
 pub mod rocksdb;
 
 pub mod disabled;
+pub mod sled;
 
 /// The basic implementation for the Key-Value DB used by this middleware
 /// This was abstracted so we could use multiple types of databases without having
@@ -19,7 +22,8 @@ pub struct KVDB {
     inner: Arc<rocksdb::RocksKVDB>,
     //TODO: This should be an else, not just not rocksdb
     #[cfg(not(feature = "persistent_db_rocksdb"))]
-    inner: disabled::DisabledKV
+    inner: sled::SledKVDB,
+    //inner: disabled::DisabledKV
 }
 
 impl KVDB {
@@ -34,7 +38,8 @@ impl KVDB {
             #[cfg(feature = "persistent_db_rocksdb")]
             {Arc::new(rocksdb::RocksKVDB::new(db_path, prefixes_cpy)?)}
             #[cfg(not(feature = "persistent_db_rocksdb"))]
-            {disabled::DisabledKV::new(db_path, prefixes_cpy)?}
+            {sled::SledKVDB::new(db_path, prefixes_cpy)?}
+            //{disabled::DisabledKV::new(db_path, prefixes_cpy)?}
         };
 
         Ok(Self {
@@ -68,6 +73,17 @@ impl KVDB {
         self.inner.exists(prefix, key)
     }
 
+    #[cfg(not(feature = "persistent_db_rocksdb"))]
+    pub fn set<T, Y>(&self, prefix: &'static str, key: T, data: Y) -> Result<()>
+    where
+        T: AsRef<[u8]>,
+        Y: AsRef<[u8]>,
+        IVec: From<Y> 
+    {
+        self.inner.set(prefix, key, data)
+    }
+
+    #[cfg(feature = "persistent_db_rocksdb")]
     pub fn set<T, Y>(&self, prefix: &'static str, key: T, data: Y) -> Result<()>
     where
         T: AsRef<[u8]>,
@@ -75,7 +91,21 @@ impl KVDB {
     {
         self.inner.set(prefix, key, data)
     }
+    #[cfg(not(feature = "persistent_db_rocksdb"))]
+    pub fn set_all<T, Y, Z>(&self, prefix: &'static str, values: T) -> Result<()>
+    where
+        T: Iterator<Item = (Y, Z)>,
+        Y: AsRef<[u8]>,
+        Z: AsRef<[u8]>,
+        IVec: From<Y>, 
+        IVec: From<Z> 
 
+    {
+        self.inner.set_all(prefix, values)
+    }
+
+    
+    #[cfg(feature = "persistent_db_rocksdb")]
     pub fn set_all<T, Y, Z>(&self, prefix: &'static str, values: T) -> Result<()>
     where
         T: Iterator<Item = (Y, Z)>,
@@ -95,6 +125,18 @@ impl KVDB {
     /// Delete a set of keys
     /// Accepts an [`&[&[u8]]`], in any possible form, as long as it can be dereferenced
     /// all the way to the intended target.
+    /// 
+    #[cfg(not(feature = "persistent_db_rocksdb"))]
+    pub fn erase_keys<T, Y>(&self, prefix: &'static str, keys: T) -> Result<()>
+    where
+        T: Iterator<Item = Y>,
+        Y: AsRef<[u8]>,
+        IVec: From<Y> 
+    {
+        self.inner.erase_keys(prefix, keys)
+    }
+
+    #[cfg(feature = "persistent_db_rocksdb")]
     pub fn erase_keys<T, Y>(&self, prefix: &'static str, keys: T) -> Result<()>
     where
         T: Iterator<Item = Y>,
@@ -102,6 +144,7 @@ impl KVDB {
     {
         self.inner.erase_keys(prefix, keys)
     }
+
 
     ///Delete a range of keys from the database
     /// Accepts the start key and the end key
@@ -126,6 +169,8 @@ impl KVDB {
         self.inner.compact_range(prefix, start, end)
     }
 
+
+    #[cfg(feature = "persistent_db_rocksdb")]
     pub fn iter(
         &self,
         prefix: &'static str,
@@ -133,7 +178,29 @@ impl KVDB {
         self.iter_range::<Vec<u8>, Vec<u8>>(prefix, None, None)
     }
 
-    pub fn iter_range<T, Y>(
+    #[cfg(not(feature = "persistent_db_rocksdb"))]
+    pub fn iter(
+        &self,
+        prefix: &'static str,
+    ) -> Result<Box<dyn Iterator<Item = Result<(Box<[u8]>, Box<[u8]>)>> + '_>> {
+        self.iter_range::<Vec<u8>>(prefix, None, None)
+    }
+
+    #[cfg(not(feature = "persistent_db_rocksdb"))]
+    pub fn iter_range<T>(
+        &self,
+        prefix: &'static str,
+        start: Option<T>,
+        end: Option<T>,
+    ) -> Result<Box<dyn Iterator<Item = Result<(Box<[u8]>, Box<[u8]>)>> + '_>>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.inner.iter_range(prefix, start, end)
+    }
+
+    #[cfg(feature = "persistent_db_rocksdb")]
+    pub fn iter_range<T,Y>(
         &self,
         prefix: &'static str,
         start: Option<T>,
---
+++ src/persistentdb/sled/mod.rs
@@ -0,0 +1,206 @@
+use std::{path::Path};
+use sled::{Config, Db, Tree, IVec, Batch};
+use anyhow::{anyhow, Context};
+use std::fmt::format;
+use crate::error::*;
+use crate::Err;
+use crate::persistentdb::PersStorage;
+
+#[derive(Debug,Clone)]
+pub(crate) struct SledKVDB {
+    db: Db,
+}
+
+impl SledKVDB {
+    pub fn new<T>(db_location: T, prefixes: Vec<&'static str>) -> Result<Self>
+    where
+        T: AsRef<Path>,
+    {
+        let conf = Config::default()
+        .mode(sled::Mode::HighThroughput)
+        .path(db_location)
+        .cache_capacity(2*1024*1024*1024);
+
+        let db = conf.open().unwrap();
+
+        for tree in prefixes {
+
+            let _ = db.open_tree(tree);
+        }
+       
+
+        Ok(SledKVDB { db })
+    }
+
+    fn get_handle(&self, prefix: &'static str) -> Result<Tree> {     
+           let handle = self.db.open_tree(prefix);       
+
+        if let Ok(handle) = handle {
+            Ok(handle)
+        } else {
+            Err!(PersStorage::NoPrefix(prefix))
+        }
+    }
+
+    pub fn get<T>(&self, prefix: &'static str, key: T) -> Result<Option<Vec<u8>>>
+    where
+        T: AsRef<[u8]>,
+    {
+        let tree = self.get_handle(prefix)?;
+
+        if let Ok(res) = tree.get(key) {
+            return Ok(res.map(|ivec| ivec.to_vec()));
+        } else { 
+            Err!(PersStorage::NoPrefix(prefix))
+        }
+
+    }
+
+    pub fn get_all<T, Y>(&self, keys: T) -> Result<Vec<Result<Option<Vec<u8>>>>>
+    where
+        T: Iterator<Item = (&'static str, Y)>,
+        Y: AsRef<[u8]>,
+    {
+        let mut values = Vec::new();
+        let final_keys =
+            keys.map(|(prefix, key)| (self.get_handle(prefix).expect("Failed to get handle"), key));
+
+        for (tree,key) in final_keys {
+            if let Ok(res) = tree.get(key) {
+              values.push(Ok(res.map(|ivec| ivec.to_vec())));
+            }
+        }
+
+        Ok(values) 
+    }
+
+    pub fn exists<T>(&self, prefix: &'static str, key: T) -> Result<bool>
+    where
+        T: AsRef<[u8]>,
+    {
+        let handle = self.get_handle(prefix)?;
+
+        if let Ok(res) = handle.contains_key(key) {
+            Ok(res)
+        } else {
+            Err!(PersStorage::NoPrefix(prefix))
+        }
+        
+    }
+
+    pub fn set<T, Y>(&self, prefix: &'static str, key: T, data: Y) -> Result<()>
+    where
+        T: AsRef<[u8]>,
+        Y: AsRef<[u8]>,
+        IVec: std::convert::From<Y>
+    {
+        let handle = self.get_handle(prefix)?;
+
+        if let Ok(_) = handle.insert(key, data) {
+            Ok(())
+        } else {
+            Err!(PersStorage::NoPrefix(prefix))
+        }
+    }
+
+    pub fn set_all<T, Y, Z>(&self, prefix: &'static str, values: T) -> Result<()>
+    where
+        T: Iterator<Item = (Y, Z)>,
+        Y: AsRef<[u8]>,
+        Z: AsRef<[u8]>,
+        IVec: std::convert::From<Z>,
+        IVec: std::convert::From<Y>,
+
+    {
+        let handle = self.get_handle(prefix)?;
+
+       let mut batch = Batch::default();
+
+        for (key, value) in values {
+            batch.insert(key, value);
+        }
+
+        let ret = handle.apply_batch(batch).context(format!("Failed to set keys"));
+        
+        //handle.flush();
+
+        ret
+    }
+
+    pub fn erase<T>(&self, prefix: &'static str, key: T) -> Result<()>
+    where
+        T: AsRef<[u8]>,
+    {
+        let handle = self.get_handle(prefix)?;
+
+
+        handle.remove(key).map(|_| ()).context(format!("Failed to erase key in prefix {:?}", prefix))
+    }
+
+    /// Delete a set of keys
+    /// Accepts an [`&[&[u8]]`], in any possible form, as long as it can be dereferenced
+    /// all the way to the intended target.
+    pub fn erase_keys<T, Y>(&self, prefix: &'static str, keys: T) -> Result<()>
+    where
+        T: Iterator<Item = Y>,
+        Y: AsRef<[u8]>,
+        IVec: std::convert::From<Y>,
+    {
+        let handle = self.get_handle(prefix)?;
+
+        let mut batch = Batch::default();
+
+        for key in keys {
+            batch.remove(key)
+        }
+
+        handle.apply_batch(batch).context(format!("Failed to erase in prefix {:?}", prefix))
+    }
+
+    pub fn erase_range<T>(&self, prefix: &'static str, start: T, end: T) -> Result<()>
+    where
+        T: AsRef<[u8]>,
+    {
+        let handle = self.get_handle(prefix)?;
+        let iter = handle.range(start..end);
+        let _ = iter.map(|res| handle.remove(res.expect("Kv pair not found").0));
+        Ok(())
+    }
+
+    pub fn compact_range<T, Y>(
+        &self,
+        prefix: &'static str,
+        start: Option<T>,
+        end: Option<Y>,
+    ) -> Result<()>
+    where
+        T: AsRef<[u8]>,
+        Y: AsRef<[u8]>,
+    {
+
+        Ok(())
+    }
+
+    pub fn iter_range<T>(
+        &self,
+        prefix: &'static str,
+        start: Option<T>,
+        end: Option<T>,
+    ) -> Result<Box<dyn Iterator<Item = Result<(Box<[u8]>, Box<[u8]>)>> + '_>>
+    where
+        T: AsRef<[u8]>,
+    {
+        let handle = self.get_handle(prefix)?;
+        let iter = match (start, end) {
+            (None, None) => handle.iter(),
+            (None, Some(end)) => handle.range(..end),
+            (Some(start), None) => handle.range(start..),
+            (Some(start), Some(end)) => handle.range(start..end),
+        };
+
+        Ok(Box::new(iter.map(|r| {
+            r.map(|(k, v)| (k.to_vec().into_boxed_slice(), v.to_vec().into_boxed_slice()))
+            .with_context(|| format!("Failed to erase in range"))
+        })))
+    }
+}

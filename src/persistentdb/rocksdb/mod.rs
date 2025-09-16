c878c1b88dbc8b4e82e267a188817972558bca68
Amadeu Marques <maramadeu@gmail.com>
2025-09-16 16:34:14 +0100

    rocksdb config

--- src/persistentdb/rocksdb/mod.rs
+++ src/persistentdb/rocksdb/mod.rs
@@ -27,12 +27,11 @@ impl RocksKVDB {
             let mut cf_opts = Options::default();
 
             if cf.eq("state") {
-                cf_opts.increase_parallelism(48);
+                cf_opts.increase_parallelism(12);
                 print!("state");
                 cf_opts.set_enable_blob_files(true);
-                cf_opts.set_blob_file_size(0x4000000);
-                cf_opts.set_write_buffer_size(0x4000000);
-                cf_opts.set_min_blob_size(0x200000);
+                cf_opts.set_blob_file_size(0x2000000);
+                cf_opts.set_min_blob_size(0x100000);
              //   cf_opts.set_target_file_size_base(0x200000);
               //  cf_opts.set_max_bytes_for_level_base(0x2000000);
                 //cf_opts.prepare_for_bulk_load();

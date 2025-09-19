1478cd2d04f8d445dd1849a67bb96cc3d3c35686
Amadeu Marques <amadeu.marques@adyta.pt>
2025-09-19 15:24:48 +0000

    change rocksdb settings

--- src/persistentdb/rocksdb/mod.rs
+++ src/persistentdb/rocksdb/mod.rs
@@ -26,15 +26,13 @@ impl RocksKVDB {
         for cf in prefixes {
             let mut cf_opts = Options::default();
 
-            if cf.eq("state") {
-                cf_opts.increase_parallelism(12);
+             if cf.eq("state") {
+                cf_opts.increase_parallelism(48);
                 print!("state");
                 cf_opts.set_enable_blob_files(true);
-                cf_opts.set_blob_file_size(0x2000000);
-                cf_opts.set_min_blob_size(0x100000);
-             //   cf_opts.set_target_file_size_base(0x200000);
-              //  cf_opts.set_max_bytes_for_level_base(0x2000000);
-                //cf_opts.prepare_for_bulk_load();
+                cf_opts.set_blob_file_size(0x4000000);
+                cf_opts.set_write_buffer_size(0x4000000);
+                cf_opts.set_min_blob_size(0x200000);
             }
 
             cfs.push(ColumnFamilyDescriptor::new(cf, cf_opts));
